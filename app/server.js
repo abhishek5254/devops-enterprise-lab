450ac421ab98022836d272c2151fc6ce34f2ae69
abhishekup7752@gmail.com <root@SMLL2212019.sml.com>
2026-02-25 06:14:46 +0000

    final code for server.js

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
- res.send("Enterprise DevOps Pipeline Running")
+ res.send("This is abhishek learning CI and CD from scratch, If you are seeing this page that means he is qualified as being a devops enginner --> Enterprise DevOps Pipeline Running")
 })
 
 app.listen(3000)
