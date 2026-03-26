9c6f0197ae72f66b86288297aeb1f2bb5353a1d1
abhishekup7752@gmail.com <root@SMLL2212019.sml.com>
2026-03-26 10:01:52 +0000

    added some changes

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
- res.send("Note: - This is abhishek learning CI and CD from scratch, If you are seeing this page that means he is qualified as being a devops enginner --> Enterprise DevOps Pipeline Running")
+ res.send("Note: - This is abhishek upadhyay learning CI and CD from scratch, If you are seeing this page that means he is qualified as being a devops enginner, this is being used to test whether pipeline is getting triggered after any changes is being done to the githu part --> Enterprise DevOps Pipeline Running")
 })
 
 app.listen(3000)
